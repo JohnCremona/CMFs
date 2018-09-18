4cbef7d54b7ba3c69fe89db9cb5cdaa6c9458299
Edgar Costa <edgarcosta@math.dartmouth.edu>
2018-09-18 15:29:53 -0400

    typo

--- ccscripts/upsert_embeddings.py
+++ ccscripts/upsert_embeddings.py
@@ -1,4 +1,4 @@
-from sage.all import  vector, PolynomialRing, ZZ, NumberField, parallel
+from sage.all import  vector, PolynomialRing, ZZ, NumberField, RDF
 import os, sys
 os.chdir('/home/edgarcosta/lmfdb/')
 sys.path.append('/home/edgarcosta/lmfdb/')
@@ -61,8 +61,8 @@ def upsert_embedding(id_number):
 
         for i, elt in enumerate(qexp_diff):
             if elt == min_diff:
-                row_embeddings['embedding_root_real'] = embeddings[i](HF.gen()).real()
-                row_embeddings['embedding_root_imag'] = embeddings[i](HF.gen()).imag()
+                row_embeddings['embedding_root_real'] = RDF(embeddings[i](HF.gen()).real())
+                row_embeddings['embedding_root_imag'] = RDF(embeddings[i](HF.gen()).imag())
                 break
     assert len(row_embeddings) == 2
     db.mf_hecke_cc.upsert({'id': rowcc['id']}, row_embeddings)
