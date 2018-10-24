ccb9dfbd5f51946cfd224ddea86c2da9f2b47be7
Edgar Costa <edgarc@mit.edu>
2018-10-24 10:11:53 -0400

    trace hash for dim < 20

--- ccscripts/upsert_trace_hash.py
+++ ccscripts/upsert_trace_hash.py
@@ -7,13 +7,19 @@ from  lmfdb.db_backend import db
 
 
 def upsert_trace_hash(id_number):
-    newform = db.mf_newforms.lucky({'id':id_number}, projection=['label','trace_hash'])
+    newform = db.mf_newforms.lucky({'id':id_number}, projection=['label','trace_hash','dim'])
     if newform is None:
+        return
+    if newform.get('trace_hash',None) is None:
+        assert newform['dim'] > 20
         return
     base_url = "ModularForm/GL2/Q/holomorphic/" + newform['label'].replace(".","/")
     trace_hash = newform['trace_hash']
     Lhash = db.lfunc_instances.lucky({'url': base_url}, projection='Lhash')
-    assert Lhash is not None
+    if Lhash is None:
+        assert newform['dim'] > 80
+        return
+    assert Lhash is not None, 'url = %s' % base_url
     db.lfunc_lfunctions.upsert({'Lhash': Lhash}, {'trace_hash' : trace_hash})
 
 import sys
