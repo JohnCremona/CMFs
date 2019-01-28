1f167f3ba5e9d635ae1bca710ab8ea72aac25838
Edgar Costa <edgarcosta@math.dartmouth.edu>
2019-01-28 14:28:49 -0500

    careful CCC

--- ccscripts/orbits-traces-LFs.py
+++ ccscripts/orbits-traces-LFs.py
@@ -642,8 +642,8 @@ def do(level, weight, lfun_filename = None, instances_filename = None, hecke_fil
         j = result['j']
         offset = 0
         coeffblob = result['coefficients']
-        exponent = result['exponent']
-        prec = result['prec']
+        exponent = int(result['exponent'])
+        prec = int(result['prec'])
         # print prec, exponent
         _coeffs = [CCC(0)] * (to_compute + 1)
         #for k in range(35): # number of prime powers < 100
