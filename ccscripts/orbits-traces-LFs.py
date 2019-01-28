045aec70b372edd85d8ca1658d5cd23e76a76f93
Edgar Costa <edgarcosta@math.dartmouth.edu>
2019-01-28 13:28:31 -0500

    zeros as ZZ

--- ccscripts/orbits-traces-LFs.py
+++ ccscripts/orbits-traces-LFs.py
@@ -775,7 +775,7 @@ def do(level, weight, lfun_filename = None, instances_filename = None, hecke_fil
             zdata = struct.unpack_from("B" * nlimbs, result['zeros'], offset)
             offset = offset + nlimbs
             z = sum( [x * 2**(8*k) for (k, x) in enumerate(reversed(zdata))] )
-            _zeros.append(z)
+            _zeros.append(ZZ(z))
         zeros[(chi,j)] = _zeros
         Ldbresults[(chi,j)] = result
 
