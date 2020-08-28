9c37b4e53a59dc7ca307bc98cfdcffdef9dff1d6
John Cremona <john.cremona@gmail.com>
2020-08-28 12:36:40 -0400

    sage-pari code update for reading mf output from Magma

--- sage-pari/mf_compare.py
+++ sage-pari/mf_compare.py
@@ -155,7 +155,7 @@ def read_eigdata(s, key, debug=False):
     # s = eval(s)
     # return [{'poly':si[0], 'basis':si[1], 'n':si[2], 'm':si[3], 'ans':si[4]} for si in s]
 
-def read_dtp(fname):
+def read_dtp(fname, verbose=True):
     # read full data: N:k:i:t:dims:traces:ALs:polys:cutters:eigdata:cm:it:pra
     data = {}
     max_time = tot_time = tot_time0 = 0.0
@@ -175,7 +175,7 @@ def read_dtp(fname):
         key = (N,k,chi)
         #print(key)
         if key in data:
-            print("Duplicate data for {}".format(key))
+            print("Duplicate data for {} in file {}".format(key, fname))
         t=float(fields[3])
         if t>max_time:
             max_time = t
@@ -197,13 +197,14 @@ def read_dtp(fname):
             tot_time0 += t
     alldims=list(set(alldims))
     alldims.sort()
-    print("Read {} spaces of which {} are nontrivial; {} Galois orbits.".format(nspaces, nspaces0, norbits))
-    print("{} orbits have dimension <=20".format(n20))
-    print("largest three dimensions: {}".format(alldims[-3:]))
-    print("Total time = {:0.3f}".format(tot_time))
-    print("Max time = {:0.3f} for space {}".format(max_time, max_space))
-    print("Average time (all spaces)      = {:0.3f}".format(tot_time/nspaces))
-    print("Average time (nonzero spaces)  = {:0.3f}".format(tot_time0/nspaces0))
+    if verbose:
+        print("Read {} spaces of which {} are nontrivial; {} Galois orbits.".format(nspaces, nspaces0, norbits))
+        print("{} orbits have dimension <=20".format(n20))
+        print("largest three dimensions: {}".format(alldims[-3:]))
+        print("Total time = {:0.3f}".format(tot_time))
+        print("Max time = {:0.3f} for space {}".format(max_time, max_space))
+        print("Average time (all spaces)      = {:0.3f}".format(tot_time/nspaces))
+        print("Average time (nonzero spaces)  = {:0.3f}".format(tot_time0/nspaces0))
     return data
 
 def file_stats(fname, dmax=20):
