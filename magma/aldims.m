8c9cfab99e2399ee0ef0a2784cba6fb40b923a22
Eran Assaf <assaferan@gmail.com>
2025-04-11 13:31:58 -0400

    added computation via traces of operators computed on modular symbols spaces

--- magma/aldims.m
+++ magma/aldims.m
@@ -44,14 +44,37 @@ function ALdimsModSym(N, k)
     return dims;
 end function;
 
-/*
-function ALDimensionModSymTraces(sgns, N, k)
+function ALdimsModSymTraces(N, k)
     assert k ge 2; // Modular symbols would not work for k=1
     Mk := ModularSymbols(N,k);
     Sk := CuspidalSubspace(Mk);
     Snew := NewSubspace(Sk);
+    Sold := OldSubspace(Sk);
     Ek := EisensteinSubspace(Mk);
-    tr_Mk := &+[(-1)^sgns[i]*Trace(AtkinLehnerOperator(Mk, p)) : p in PrimeDivisors(N)];
-    tr_Mk +:= Dimension(Mk);
+    // These two lines require suppressing a requirement in Magma's two intrinsics, to be added in future Magma version
+    Enew := NewSubspace(Ek);
+    Eold := OldSubspace(Ek);
+    // We compute the redundant Ek, Sk and Mk for verification
+    spaces := [Snew, Sold, Enew, Eold, Sk, Ek, Mk];
+    keys := ["cusp_new", "cusp_old", "eis_new", "eis_old", "cusp", "eis", "full"];
+    dims := AssociativeArray();
+    for i->V in spaces do
+        als := [AtkinLehnerOperator(V,p) : p in PrimeDivisors(N)];
+        V_dims := [Integers() | &+[&*[Integers() | sgns[j] : j in J]*Trace(&*[Universe(als) | als[j] : j in J]) : J in Subsets({1..#als})] / 2^(#als) 
+                    : sgns in CartesianPower([1,-1],#als)];
+        dims[keys[i]] := V_dims;
+    end for;
+    for i in [1..2^#PrimeDivisors(N)] do
+        assert dims["cusp_new"][i] + dims["cusp_old"][i] eq dims["cusp"][i];
+        assert dims["eis_new"][i] + dims["eis_old"][i] eq dims["eis"][i];
+        assert dims["cusp"][i] + dims["eis"][i] eq dims["full"][i];
+    end for;
+    Remove(~dims, "cusp");
+    Remove(~dims, "full");
+    Remove(~dims, "eis");
+    for key in ["cusp_new", "cusp_old"] do
+        assert &and[IsEven(d) : d in dims[key]];
+        dims[key] := [d div 2 : d in dims[key]];
+    end for;
+    return dims;
 end function;
-*/
